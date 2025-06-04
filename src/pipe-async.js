9288665ea36b60e4da8fb6db464a0248c4a910c3
Matt Riley <m@ttriley.dev>
2025-06-04 18:48:47 +1000

    Unspecified changes

--- src/pipe-async.js
+++ src/pipe-async.js
@@ -1,8 +1,8 @@
 const isPlainFunction = require('./is-plain-function');
 
-module.exports = () => (...funcs) => initial => {
+module.exports = (...funs) => initial => {
 
-    return funcs.reduce(async (p, f) => {
+    return funs.reduce(async (p, f) => {
         const acc = await p;
         return (isPlainFunction(f) ? f(acc) : f) ?? acc; // return undefined to return last acc.
     }, initial);
